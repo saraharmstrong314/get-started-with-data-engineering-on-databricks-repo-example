f7ba4187b6f554c7ae0286fdbc361a0889eb513f
saraharmstrong314 <saraharmstrong314>
2024-07-24 22:42:23 +0000

    Yeet

---
+++ Databricks tutorial thing 2024-07-24 15_07_32.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### This is my first notebook and I hope the connected sign doesn't mean this is costing resources so if I can't figure how to turn it off I guess I'll just stick to watching the demos on Databricks Academy.
+
+# COMMAND ----------
+
+
